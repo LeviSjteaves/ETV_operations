031d406ccfa7704e8e514e1dee796898f0f0f24f
LeviSjteaves <l.a.stevens@student.tue.nl>
2023-10-02 09:37:44 +0200

    First python scipt
    Pyton script which loads airport and determines runway nodes

---
+++ Extract_map.m
@@ -0,0 +1,15 @@
+% % Define the geographic bounding box for the city (latitude and longitude)
+% lat = [52.2864, 52.3643];
+% lon = [4.7042, 4.8144];
+% 
+% % Download OpenStreetMap data for the city
+% osmData = osm.download(lat, lon);
+% % Create a street network graph from the downloaded data
+% streetGraph = osm.create(osmData, 'show', false);
+% % Plot the street network graph
+% figure;
+% plot(streetGraph, 'Layout', 'layered');
+
+openstreetmap_filename = 'map.osm';
+
+
