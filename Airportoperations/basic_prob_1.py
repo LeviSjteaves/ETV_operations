b1d5781f464f0d68a03748bee7c11888969728d2
LeviSjteaves <l.a.stevens@student.tue.nl>
2023-11-20 16:34:22 +0100

    Working optimization model
    Start time of task not able to 'wait' on available etv due to using if statement in precalculation.
    The order of towing tasks and collisions not yet defined.
    Energy consumption for ETV's not included.

--- Airportoperations/basic_prob_1.py
+++ Airportoperations/basic_prob_1.py
@@ -22,20 +22,19 @@ def create_model(N_aircraft, P, N_etvs, max_speed_a, min_speed_a):
                 for m in range(1, len(P[b])):
                     e_col = []
                     #possibility to be at the same node at the same time
-                    if a != b and P[a][n] == P[b][m] and (t_min[a][n] <= t_max[b][m] and t_min[b][m] <= t_max[a][n]):
+                    if a < b and P[a][n] == P[b][m] and (t_min[a][n] <= t_max[b][m] and t_min[b][m] <= t_max[a][n]):
                         e_col = [a,b]
                         I_col_nodes.append(P[a][n])
                         I_col.append(e_col)
     idx_col = []
     for a in range(len(I_col_nodes)):
         for b in range(len(I_col_nodes)):
-            if I_col[a][0] == I_col[b][1] and I_col[a][1] == I_col[b][0] and I_col_nodes[a] == I_col_nodes[b] and a<b:
+            if I_col[a][0] == I_col[b][1] and I_col[a][1] == I_col[b][0] and I_col_nodes[a] == I_col_nodes[b]:
                 idx_col.append([a,b])
                 
     # Decision variables
     t = {}  # Arrival times nodes
     X = {}  # Towing tasks
-    Z = {}  # Order of solving collisions
     
     for a in range(N_aircraft):
         for n in range(len(P[a])):
@@ -44,12 +43,9 @@ def create_model(N_aircraft, P, N_etvs, max_speed_a, min_speed_a):
     for a in range(N_aircraft):
         for i in range(N_etvs):
             X[a, i] = model.addVar(vtype=GRB.BINARY, name=f"X_{a}_{i}")
-            
-    for a in range(len(idx_col)):
-            Z[a] = model.addVar(vtype=GRB.BINARY, name=f"Z_{a}")
-            
+                        
     # Objective function: minimize emissions (only rolling resistance) + total taxitime
-    model.setObjective(gp.quicksum(mu*m_a*g*d_a[a]*eta*(1-sum(X[a,i] for i in range(N_etvs))) for a in range(N_aircraft))+gp.quicksum(t[a, n] for a in range(N_aircraft) for n in range(1, len(P[a])))+100000000*(1-gp.quicksum(Z[a] for a in range(len(idx_col)))), sense=GRB.MINIMIZE)
+    model.setObjective(gp.quicksum(mu*m_a*g*d_a[a]*eta*(1-sum(X[a,i] for i in range(N_etvs))) for a in range(N_aircraft))+gp.quicksum(t[a, n] for a in range(N_aircraft) for n in range(1, len(P[a]))), sense=GRB.MINIMIZE)
     
                          
     # Constraints
@@ -63,30 +59,19 @@ def create_model(N_aircraft, P, N_etvs, max_speed_a, min_speed_a):
             model.addConstr(t[a, n] >= t[a, n-1] + short_path_dist(G_a, P[a][n-1], P[a][n]) / max_speed_a, f"arrival_constraint_min_{a}_{n}")
             model.addConstr(t[a, n] <= t[a, n-1] + short_path_dist(G_a, P[a][n-1], P[a][n]) / min_speed_a, f"arrival_constraint_max_{a}_{n}")
     
-    # Collision
-    I_col_filter = []
-    I_col_nodes_filter = []
-    idx_choice=[0,1]    
-    idx_ind = [0]*len(idx_col)
-    for a in range(len(idx_col)):
-        model.addConstr(idx_ind[a] == Z[a] * idx_choice[1] + (1 - Z[a]) * idx_choice[0], f"order_constraint_{a}")
-        I_col_filter.append(I_col[idx_col[a][idx_ind[a]]])
-        I_col_nodes_filter.append(I_col_nodes[idx_col[a][idx_ind[a]]])
-    
-    for n in range(len(I_col_nodes_filter)):
-            model.addConstr(t[I_col_filter[n][1],P[I_col_filter[n][1]].index(I_col_nodes_filter[n])] >= t[I_col_filter[n][0],P[I_col_filter[n][0]].index(I_col_nodes_filter[n])]+1, f"collision_constraint_node_{a}_{n}")
-    
+    # Collision   
+    for n in range(len(I_col_nodes)):
+            model.addConstr(t[I_col[n][1],P[I_col[n][1]].index(I_col_nodes[n])] >= t[I_col[n][0],P[I_col[n][0]].index(I_col_nodes[n])]+5, f"collision_constraint_node_{a}_{n}")
+                
     # ETV constraints availability
-    for k in range(len(I_col_filter)):  
-        a =I_col_filter[k][0]
-        b =I_col_filter[k][1]
-        for i in range(N_etvs):
-            if tO_a[b] <= tO_a[a] +short_path_dist(G_a, O_a[a], D_a[a]) / max_speed_a + short_path_dist(G_e, D_a[a], O_a[b]) / speed_e: #assume fastest path instead of actual path time
-                    model.addConstr(X[a, i]+ X[b, i] <= 1)
-                    #e=1
-
+    for a in range(N_aircraft):
+       for b in range(N_aircraft):
+           for i in range(N_etvs):
+               if tO_a[b] <= tO_a[a] +short_path_dist(G_a, O_a[a], D_a[a]) / max_speed_a + short_path_dist(G_e, D_a[a], O_a[b]) / speed_e and a<b:
+                   model.addConstr(X[a,i]+X[b,i] <=1 , f"available_etv_constraint_{a}_{n}") #assume fastest path instead of actual path time
+    
     model.update()
-    return model, I_col, I_col_nodes, idx_col, idx_ind, I_col_nodes_filter
+    return model, I_col, I_col_nodes, idx_col
 
 def short_path_dist(G, n1, n2):
     dist = nx.shortest_path_length(G, source=n1, target=n2, weight='weight')
@@ -110,13 +95,13 @@ if __name__ == "__main__":
     min_speed_a = 0.01
     O_a = [0, 1, 4]   # Origins
     D_a = [4, 5, 0]   # Destinations
-    tO_a = [50, 0, 0]  # Appearing times
+    tO_a = [0, 0, 0]  # Appearing times
     mu = 0.02      # Rolling resistance
     m_a = 40000    # Airplane mass
     eta = 0.3      # Turbine efficiency
 
     # ETV info
-    N_etvs = 1
+    N_etvs = 2
     speed_e = 3
 
     # Aircraft routing
@@ -144,7 +129,7 @@ if __name__ == "__main__":
         t_max.append(t_a_max)
     
     # Create the Gurobi model
-    model, I_col, I_col_nodes, idx_col, idx_ind, I_col_nodes_filter = create_model(N_aircraft, P, N_etvs, max_speed_a, min_speed_a)
+    model, I_col, I_col_nodes, idx_col = create_model(N_aircraft, P, N_etvs, max_speed_a, min_speed_a)
 
     # Optimize the model
     model.optimize()
